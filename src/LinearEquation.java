3828f18e676680c4d4b0e0387c15d2ba61018435
Rack e <tim@gmail.com>
2023-10-11 18:46:47 -0400

    more code

--- src/LinearEquation.java
+++ src/LinearEquation.java
@@ -9,8 +9,9 @@ private int x;
         private double roundedSlope;
         private double yIntercept;
         private double distanceX;
-        private double y3;
+
 private int x3;
+private double y3;
 public LinearEquation(int firstX, int firstY, int secondX, int secondY ){
         x=firstX;
                 y=firstY;
@@ -18,16 +19,20 @@ public LinearEquation(int firstX, int firstY, int secondX, int secondY ){
                                 y2=secondY;
 
 }
+    public LinearEquation(int thirdX){
+        x3=thirdX;
+
+    }
 
 public double calculateSlope(){
 
 
         double slope=(double)(y2-y)/(double)(x2-x);
- roundedSlope=Math.round((double) ((y2-y) / (x2-x)*100.0)/100.0);
+ roundedSlope=Math.round( (double) ((y2-y) / (x2-x)*100.0)/100.0);
 return roundedSlope;
 }
 public double calculateYIntercept() {
-        yIntercept = Math.round((y - (calculateSlope() * x)) * 100.0) / 100.0;
+        yIntercept = Math.round((y - (roundedSlope * x)) * 100.0) / 100.0;
         return yIntercept;
 }
 public double distanceBetweenPoints(){
@@ -36,18 +41,21 @@ return distanceX;
 
 }
 public String toString(){
-        String yes =  "/"+ (x2-x) + "x +" + calculateYIntercept();
-        String info=("First Coordinate: ("+x+","+y+")\nSecond Coordinate: ("+x2+","+y2+")\nSlope:"+ calculateSlope()+"\nY-Intercept:"+calculateYIntercept()+"\nDistance: "+distanceBetweenPoints()+"\nSlope Intercept Equation: y = "+(y2-y)+yes+"\n");
+        String yes =  "/"+ (x2-x) + "x +" + yIntercept;
+        String info=("First Coordinate: ("+x+","+y+")\nSecond Coordinate: ("+x2+","+y2+")\nSlope:"+ roundedSlope+"\nY-Intercept:"+yIntercept+"\nDistance: "+distanceX+"\nSlope Intercept Equation: y = "+(y2-y)+yes+"\n");
 return info;
 }
-        public LinearEquation(int thirdX){
-                thirdX=x3;
-        }
-public double getThirdY(){
-         y3=(roundedSlope*x3)+yIntercept;
+
+public double getThirdY(int thirdX){
+         y3=roundedSlope*thirdX+yIntercept;
         return y3;}
-        public void thirdCoords(){
-            System.out.println("("+x3+","+y3+")");
+
+    public void x3(){
+        System.out.print("Solved Coordinate: ("+(double)x3);
+    }
+    public void y3(){
+            System.out.println(","+y3+")");
         }
+
 }
 
--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,15 @@
 import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-Scanner s=new Scanner(System.in);
-System.out.print("Enter first xy coordinate: ");
-String first=s.nextLine();
+        Scanner s=new Scanner(System.in);
+        System.out.print("Enter first xy coordinate: ");
+        String first=s.nextLine();
         String firstNoParentheses=first.substring(1,first.length()-1);
         int noComma=firstNoParentheses.indexOf(",");
         String noCommaYesX=firstNoParentheses.substring(0,noComma);
         String noCommaYesY=firstNoParentheses.substring(noComma+1,firstNoParentheses.length());
-     int firstX=  Integer.parseInt(noCommaYesX);
-     int firstY  =Integer.parseInt(noCommaYesY);
+        int firstX=  Integer.parseInt(noCommaYesX);
+        int firstY  =Integer.parseInt(noCommaYesY);
         System.out.print("Enter second xy coordinate: ");
         String second=s.nextLine();
         String secondNoParentheses=second.substring(1,second.length()-1);
@@ -19,13 +19,18 @@ String first=s.nextLine();
         int secondX=  Integer.parseInt(secondNoCommaYesX);
         int secondY  =Integer.parseInt(secondNoCommaYesY);
         LinearEquation Equation=new LinearEquation(firstX,firstY,secondX,secondY);
+        Equation.calculateSlope();
+        Equation.calculateYIntercept();
+        Equation.distanceBetweenPoints();
         System.out.print(Equation.toString());
         System.out.print("Enter third X coordinate:");
         String third=s.nextLine();
         int thirdCoord=Integer.parseInt(third);
-        int a=thirdCoord;
-        LinearEquation Equation2=new LinearEquation(thirdCoord);
-        Equation2.thirdCoords();
+        LinearEquation Equation2= new LinearEquation(thirdCoord);
+        Equation.getThirdY(thirdCoord);
+        Equation2.x3();
+        Equation.y3();
+
 
 
 
